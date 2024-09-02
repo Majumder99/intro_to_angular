c5d72c9a2dc83b2597b3627342e5750db0472cd1
Majumder99 <souravmajumderk99@gmail.com>
2024-09-03 01:24:40 +0600

    getting the products

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Products } from '../../types';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  constructor(private productService: ProductsService) {}
 
+  ngOnInit() {
+    this.productService
+      .getProducts('http://localhost:3000/clothes', {
+        page: 0,
+        perPage: 5,
+      })
+      .subscribe((products: Products) => {
+        console.log(products);
+      });
+  }
 }

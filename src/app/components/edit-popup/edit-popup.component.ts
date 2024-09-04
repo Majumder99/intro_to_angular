24963b45cd8b61d2f32f80b9ddd467097db08dce
Majumder99 <souravmajumderk99@gmail.com>
2024-09-04 13:22:39 +0600

    learned many things from video not included those in code

---
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { Product } from '../../../types';
+@Component({
+  selector: 'app-edit-popup',
+  standalone: true,
+  imports: [DialogModule, CommonModule],
+  templateUrl: './edit-popup.component.html',
+  styleUrl: './edit-popup.component.css',
+})
+export class EditPopupComponent {
+  @Input() display: boolean = false;
+  @Output() confirm = new EventEmitter<any>();
+
+  @Input() product: Product = {
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  };
+
+  onConfirm() {
+    this.confirm.emit(this.product);
+  }
+
+  onCancel() {
+    this.display = false;
+  }
+}
